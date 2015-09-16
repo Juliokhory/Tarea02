4dfbe0ff11f3e7324b654709c1c333bb4991ae50
Julio Ortiz <jcortizojeda@gmail.com>
2015-09-16 15:50:44 -0500

    agregando archivo maps.js

---
+++ maps.js
@@ -0,0 +1,6 @@
+function iniciar() {
+var mapOptions = {
+center: new google.maps.LatLng(-17.376,-66.143),
+zoom: 17,
+mapTypeId: google.maps.MapTypeId.TERRAIN};
+var map = new google.maps.Map(document.getElementById("map"),mapOptions);}
